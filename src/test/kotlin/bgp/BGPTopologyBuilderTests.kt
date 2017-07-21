9080e86549fc20501261860be23203a63a7f4bdb
David Fialho <fialho.david@protonmail.com>
2017-07-21 12:04:36 +0100

    Refactored BGP topology builder tests

--- src/test/kotlin/bgp/BGPTopologyBuilderTests.kt
+++ src/test/kotlin/bgp/BGPTopologyBuilderTests.kt
@@ -1,11 +1,14 @@
 package bgp
 
+import core.routing.Extender
+import core.routing.NodeID
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
+import org.jetbrains.spek.api.dsl.context
 
 /**
  * Created on 21-07-2017
@@ -24,9 +27,8 @@ object BGPTopologyBuilderTests : Spek({
             }
         }
 
-        on("adding a node with ID 1 and calling build") {
+        on("calling build") {
 
-            builder.addNode(1)
             val topology = builder.build()
 
             it("returns a topology containing a single node with ID 1") {
@@ -35,10 +37,31 @@ object BGPTopologyBuilderTests : Spek({
 
         }
 
-        on("adding two nodes with IDs 1 and 2 and calling build") {
+        on("adding second node with ID 1 ") {
+            val added = builder.addNode(1)
 
-            builder.addNode(1)
-            builder.addNode(2)
+            it("returns true indicating the node was added") {
+                assertThat(added, `is`(false))
+            }
+        }
+
+        on("calling build") {
+
+            val topology = builder.build()
+
+            it("returns a topology of size 1") {
+                assertThat(topology.size, equalTo(1))
+            }
+        }
+
+        on("adding second node with ID 2") {
+            it("returns true indicating the node was added") {
+                assertThat(builder.addNode(2), `is`(true))
+            }
+        }
+
+        on("calling build") {
+
             val topology = builder.build()
 
             it("returns a topology containing two nodes with IDs 1 and 2") {
@@ -49,22 +72,24 @@ object BGPTopologyBuilderTests : Spek({
 
     }
 
-    given("a builder already containing one node with ID 1") {
-
-        val builder = BGPTopologyBuilder()
-        builder.addNode(1)
-
-        on("adding a second node with ID 1") {
-            it("returns false") {
-                assertThat(builder.addNode(1), `is`(false))
-            }
-        }
-
-        on("adding a second node with ID 2") {
-            it("returns true") {
-                assertThat(builder.addNode(2), `is`(true))
-            }
-        }
-    }
 
 })
+
+/**
+ * Returns a BGP link connecting two BGP nodes with the given IDs. The returned link is associated with a extender
+ * obtained using the someExtender() method.
+ */
+fun BGPLink(from: NodeID, to: NodeID): BGPLink {
+    return BGPLink(BGPNodeWith(from), BGPNodeWith(to), someExtender())
+}
+
+/**
+ * Returns an extender when it is needed one but it is not important which one.
+ */
+fun someExtender(): BGPExtender {
+    return FakeExtender
+}
+
+object FakeExtender : BGPExtender {
+    override fun extend(route: BGPRoute): BGPRoute = invalidBGPRoute()
+}
