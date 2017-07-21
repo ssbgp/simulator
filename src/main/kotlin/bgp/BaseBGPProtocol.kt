36bba4b814c9255ccf2d17f532548045f8e7b743
David Fialho <fialho.david@protonmail.com>
2017-07-22 00:23:08 +0100

    Defined the structure for implementing the protocols

---
+++ src/main/kotlin/bgp/BaseBGPProtocol.kt
@@ -0,0 +1,95 @@
+package bgp
+
+import com.sun.xml.internal.ws.api.message.Message
+
+/**
+ * Created on 21-07-2017
+ *
+ * @author David Fialho
+ */
+sealed class BaseBGPProtocol {
+
+    /**
+     * Flag that indicates if a new route was selected as a result of processing a new incoming message. This flag is
+     * always set to false when a new message arrives and should only be set to true if a new route is selected when
+     * the message is being processed.
+     */
+    protected var wasNewRouteSelected = false
+
+    /**
+     * Processes a BGP message received by a node.
+     * May updated the routing table and the selected route/neighbor.
+     *
+     * @param message the message to be processed
+     */
+    fun process(message: BGPMessage) {
+
+    }
+
+    /**
+     * Implements the process of importing a route.
+     *
+     * @param route    the route received by the node (route obtained directly from the message)
+     * @param extender the extender used to import the route (extender included in the message)
+     */
+    fun import(route: BGPRoute, extender: BGPExtender): BGPRoute {
+        return BGPRoute.invalid()
+    }
+
+    /**
+     * Implements the process of learning a route.
+     *
+     * @param node   the node processing the route
+     * @param sender the out-neighbor that sent the route
+     * @param route  the route imported by the node (route obtained after applying the extender)
+     */
+    fun learn(node: BGPNode, sender: BGPNode, route: BGPRoute): BGPRoute {
+        return BGPRoute.invalid()
+    }
+
+    /**
+     * Implements the process of exporting a route.
+     *
+     * @param node          the node processing the route
+     * @param selectedRoute the route imported by the node (route obtained after applying the extender)
+     */
+    fun export(node: BGPNode, selectedRoute: BGPRoute) {
+
+    }
+
+    /**
+     * Called by the protocol when it detects a routing loop.
+     */
+    protected abstract fun onLoopDetected(sender: BGPNode, route: BGPRoute)
+
+}
+
+//region Subclasses
+
+/**
+ * BGP Protocol: when a loop is detected it does nothing
+ */
+class BGPProtocol : BaseBGPProtocol() { override fun onLoopDetected(sender: BGPNode, route: BGPRoute) = Unit }
+
+/**
+ * SS-BGP Protocol: when a loop is detected it tries to detect if the loop is recurrent using the WEAK detection
+ * condition. If it determines the loop is recurrent, it disables the neighbor that exported the route.
+ */
+class SSBGPProtocol : BaseBGPProtocol() {
+
+    override fun onLoopDetected(sender: BGPNode, route: BGPRoute) {
+        TODO("not implemented")
+    }
+}
+
+/**
+ * SS-BGP Protocol: when a loop is detected it tries to detect if the loop is recurrent using the STRONG detection
+ * condition. If it determines the loop is recurrent, it disables the neighbor that exported the route.
+ */
+class ISSBGPProtocol : BaseBGPProtocol() {
+    override fun onLoopDetected(sender: BGPNode, route: BGPRoute) {
+        TODO("not implemented")
+    }
+}
+
+//endregion
---
+++ src/test/kotlin/bgp/BGPProtocolTests.kt
@@ -0,0 +1,26 @@
+package bgp
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+
+/**
+ * Created on 21-07-2017
+
+ * @author David Fialho
+ */
+object BGPProtocolLearnTests : Spek({
+
+    given("") {
+
+
+    }
+
+})
+
+object BGPProtocolProcessTests : Spek({
+
+
+})
