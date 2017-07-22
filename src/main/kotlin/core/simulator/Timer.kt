6e20b9ca27c1839813afc99b93d0555c5ded54c9
David Fialho <fialho.david@protonmail.com>
2017-07-22 14:22:55 +0100

    Implemented the Timer

--- src/main/kotlin/core/simulator/Timer.kt
+++ src/main/kotlin/core/simulator/Timer.kt
@@ -4,6 +4,9 @@ package core.simulator
  * Created on 22-07-2017
  *
  * @author David Fialho
+ *
+ * A time starts in expired mode
+ * The timer performs the specified action action whe it expires.
  */
 class Timer(val duration: Time, private val action: () -> Unit) {
 
@@ -19,13 +22,18 @@ class Timer(val duration: Time, private val action: () -> Unit) {
     @Throws(IllegalStateException::class)
     fun start() {
 
+        if (!expired) throw IllegalStateException("Can not start an expired timer")
+
+        Scheduler.scheduleFromNow(TimerExpiredEvent(this), duration)
+        expired = false
     }
 
     /**
      * Should be called when the timer expires. It calls the action and sets the timer as 'expired'.
      */
     fun onExpired() {
-
+        expired = true
+        action()
     }
 
 }
