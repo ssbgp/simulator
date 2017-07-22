592bec4e780bdae3f0591c2ef2dd63d60b475dae
David Fialho <fialho.david@protonmail.com>
2017-07-22 14:20:07 +0100

    Added the TimerExpiredEvent

---
+++ src/main/kotlin/core/simulator/TimerExpiredEvent.kt
@@ -0,0 +1,17 @@
+package core.simulator
+
+/**
+ * Created on 22-07-2017
+ *
+ * @author David Fialho
+ */
+class TimerExpiredEvent(val timer: Timer) : Event {
+
+    /**
+     * Calls the onExpired() method of the timer that expired.
+     */
+    override fun processIt() {
+        timer.onExpired()
+    }
+
+}
