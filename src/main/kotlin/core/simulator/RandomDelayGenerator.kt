b4e55726661df6cde7ed4aa8ecd293d6a7eaa52c
David Fialho <fialho.david@protonmail.com>
2017-07-22 13:57:07 +0100

    Added factory function for the random delay generator

--- src/main/kotlin/core/simulator/RandomDelayGenerator.kt
+++ src/main/kotlin/core/simulator/RandomDelayGenerator.kt
@@ -12,10 +12,33 @@ import java.util.Random
  *
  * @param min  the minimum delay value the generator will generate
  * @param max  the maximum delay value the generator will generate
- * @param seed the seed used to generate the random delays. If none is provided it uses the system current time
+ * @param seed the seed used to generate the random delays
  */
-class RandomDelayGenerator(val min: Time, val max: Time, override val seed: Long = System.currentTimeMillis())
+class RandomDelayGenerator
+private constructor(val min: Time, val max: Time, override val seed: Long)
     : DelayGenerator {
+
+    companion object Factories {
+
+        /**
+         * Returns a RandomDelayGenerator with the specified configurations.
+         *
+         * @param min  the minimum delay value the generator will generate. Must be higher than 0
+         * @param max  the maximum delay value the generator will generate. Must be higher than or equal to 'min'
+         * @param seed the seed used to generate the random delays. If none is provided it uses the system current time
+         * @throws IllegalStateException if 'max' is lower than 'min' or if 'min' is lower than 0.
+         */
+        @Throws(IllegalStateException::class)
+        fun with(min: Time, max: Time, seed: Long = System.currentTimeMillis()): RandomDelayGenerator {
+
+            if (min < 0 || max < min) {
+                throw IllegalArgumentException("Maximum delay can not be lower than minimum and minimum must be a " +
+                        "non-negative value")
+            }
+
+            return RandomDelayGenerator(min, max, seed)
+        }
+    }
 
     /**
      * Random value generator used to generate the delays
