640b16e7ae30c8bd77e6e5f4c0bb5a69b21ef384
David Fialho <fialho.david@gmail.com>
2017-07-24 11:14:59 +0100

    Added Exporter to the Relationship class

--- src/main/kotlin/core/routing/Relationship.kt
+++ src/main/kotlin/core/routing/Relationship.kt
@@ -1,8 +1,10 @@
 package core.routing
 
+import core.simulator.Exporter
+
 /**
  * Created on 20-07-2017
  *
  * @author David Fialho
  */
-data class Relationship<out N: Node, R: Route>(val node: N, val extender: Extender<R>)
+data class Relationship<out N: Node, R: Route>(val node: N, val extender: Extender<R>, val exporter: Exporter)
