3b6ee8fc41bdea9d343375ede6f3c250df07e7a6
David Fialho <fialho.david@protonmail.com>
2017-07-25 15:48:00 +0100

    Added some documentation to BGPTopology

--- src/main/kotlin/bgp/BGPTopology.kt
+++ src/main/kotlin/bgp/BGPTopology.kt
@@ -9,15 +9,34 @@ import core.routing.Topology
  * Created on 21-07-2017
  *
  * @author David Fialho
+ *
+ * A BGP topology connects BGPNodes that use a BGP like protocol to communicate. Nodes participating in this kind of
+ * protocols exchange BGPRoutes between each other.
  */
 class BGPTopology(private val nodes: Map<NodeID, BGPNode>) : Topology<BGPNode, BGPRoute> {
 
+    /**
+     * Returns the number of BGP nodes in the topology.
+     */
     override val size: Int = nodes.size
 
+    /**
+     * Returns the BGP identified by the specified ID. It returns null if the topology does not hold any node with
+     * the specified ID.
+     */
     override fun getNode(id: NodeID): BGPNode? = nodes[id]
 
+    /**
+     * Returns an immutable collection containing all nodes in the topology.
+     */
     override fun getNodes(): Collection<BGPNode> = nodes.values
 
+    /**
+     * Returns a collection containing all links between nodes in the topology.
+     *
+     * Due to the way links are stored in this type of topology, this is a time consuming operation. Try to avoid
+     * having to use it.
+     */
     override fun getLinks(): Collection<Link<BGPNode, BGPRoute>> {
 
         val links = ArrayList<BGPLink>()
@@ -31,8 +50,17 @@ class BGPTopology(private val nodes: Map<NodeID, BGPNode>) : Topology<BGPNode, B
         return links
     }
 
+    /**
+     * Returns the number of BGP nodes in the topology.
+     */
     override fun nodeCount() = size
 
+    /**
+     * Returns the number of links in the topology.
+     *
+     * Due to the way links are stored in this type of topology, this is a time consuming operation. Try to avoid
+     * having to use it.
+     */
     override fun linkCount(): Int = nodes.flatMap { it.value.relationships }.count()
 
 }
