962e68192725cacfa98bf7f582f85e0f2093e1f7
David Fialho <fialho.david@protonmail.com>
2017-08-31 11:27:04 +0100

    Add helper extension funciton toNonNegativeInt()

---
+++ src/main/kotlin/utils/HelperFunctions.kt
@@ -0,0 +1,26 @@
+package utils
+
+/**
+ * Created on 31-08-2017
+ *
+ * @author David Fialho
+ *
+ * This file contains a set of helper functions and extension function.
+ */
+
+/**
+ * Parses a string as a non-negative integer number and returns the result
+ *
+ * @return the non-negative integer
+ * @throws NumberFormatException - if the string is not a valid representation of a number.
+ */
+@Throws(NumberFormatException::class)
+fun String.toNonNegativeInt(): Int {
+
+    val value = this.toInt()
+    if (value < 0) {
+        throw NumberFormatException()
+    }
+
+    return value
+}
