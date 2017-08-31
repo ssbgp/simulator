5801d7181b3dd65ef926cc0ad8dfabfce4e09dbe
David Fialho <fialho.david@protonmail.com>
2017-08-31 11:26:52 +0100

    Add function parseInterdomainExtender()

---
+++ src/main/kotlin/io/ExtenderParseFunctions.kt
@@ -0,0 +1,43 @@
+package io
+
+import bgp.BGPRoute
+import bgp.policies.interdomain.*
+import core.routing.Extender
+
+/**
+ * Created on 31-08-2017
+ *
+ * @author David Fialho
+ *
+ * This file contains functions to parse extenders from labels.
+ */
+
+/**
+ * Parses an Interdomain Extender. The supported labels are:
+ *
+ *  R+ - parsed as a PeerplusExtender
+ *  C  - parsed as a CustomerExtender
+ *  R  - parsed as a PeerExtender
+ *  P  - parsed as a ProviderExtender
+ *  S  - parsed as a SiblingExtender
+ *
+ * This function is NOT case sensitive!
+ *
+ * @param label      the label of the extender
+ * @param lineNumber the number of the line in which the label was found (used for the parse exception message only)
+ * @return the extender parsed from the label
+ * @throws ParseException if the label is not recognized
+ */
+@Throws(ParseException::class)
+fun parseInterdomainExtender(label: String, lineNumber: Int = 0): Extender<BGPRoute> {
+
+    return when (label.toLowerCase()) {
+        "r+" -> PeerplusExtender
+        "c" -> CustomerExtender
+        "r" -> PeerExtender
+        "p" -> ProviderExtender
+        "s" -> SiblingExtender
+        else -> throw ParseException("Extender label `$label` was not recognized: " +
+                "must be either R+, C, R, P, or S", lineNumber)
+    }
+}
