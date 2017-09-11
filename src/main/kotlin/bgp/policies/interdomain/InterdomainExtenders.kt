b9234766bc8941ffc616214f5115e13baee2f977
David Fialho <fialho.david@protonmail.com>
2017-09-11 18:29:36 +0100

    Add support to Interdomain policies for peer* neighbors

--- src/main/kotlin/bgp/policies/interdomain/InterdomainExtenders.kt
+++ src/main/kotlin/bgp/policies/interdomain/InterdomainExtenders.kt
@@ -10,68 +10,81 @@ import core.routing.Node
  * @author David Fialho
  */
 
-const val LOCAL_PREF_PEERPLUS: Int = 400000
+const val LOCAL_PREF_PEERPLUS: Int = 500000
+const val LOCAL_PREF_PEERSTAR: Int = 400000
 const val LOCAL_PREF_CUSTOMER: Int = 300000
 const val LOCAL_PREF_PEER: Int = 200000
 const val LOCAL_PREF_PROVIDER: Int = 100000
 
-object CustomerExtender : Extender<BGPRoute> {
+object CustomerExtender: Extender<BGPRoute> {
 
     override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
 
         return when {
-            route.localPref <= LOCAL_PREF_PEER -> BGPRoute.invalid()
-            else                               -> customerRoute(asPath = route.asPath.append(sender))
+            route.localPref <= LOCAL_PREF_PEER || route.localPref == LOCAL_PREF_PEERSTAR  -> BGPRoute.invalid()
+            else -> customerRoute(asPath = route.asPath.append(sender))
         }
     }
 
 }
 
-object PeerExtender : Extender<BGPRoute> {
+object PeerExtender: Extender<BGPRoute> {
 
     override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
 
         return when {
-            route.localPref <= LOCAL_PREF_PEER -> BGPRoute.invalid()
-            else                               -> peerRoute(asPath = route.asPath.append(sender))
+            route.localPref <= LOCAL_PREF_PEER || route.localPref == LOCAL_PREF_PEERSTAR  -> BGPRoute.invalid()
+            else -> peerRoute(asPath = route.asPath.append(sender))
         }
     }
 
 }
 
-object ProviderExtender : Extender<BGPRoute> {
+object ProviderExtender: Extender<BGPRoute> {
 
     override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
 
         return when {
             !route.isValid() -> BGPRoute.invalid()
-            else             -> providerRoute(asPath = route.asPath.append(sender))
+            else -> providerRoute(asPath = route.asPath.append(sender))
         }
     }
 
 }
 
-object PeerplusExtender : Extender<BGPRoute> {
+object PeerplusExtender: Extender<BGPRoute> {
 
     override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
 
         return when {
-            route.localPref <= LOCAL_PREF_PEER -> BGPRoute.invalid()
-            else                               -> peerplusRoute(asPath = route.asPath.append(sender))
+            route.localPref <= LOCAL_PREF_PEER || route.localPref == LOCAL_PREF_PEERSTAR  -> BGPRoute.invalid()
+            else -> peerplusRoute(asPath = route.asPath.append(sender))
         }
     }
 
 }
 
-object SiblingExtender : Extender<BGPRoute> {
+object PeerstarExtender: Extender<BGPRoute> {
 
     override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
 
         return when {
-            !route.isValid()          -> BGPRoute.invalid()
+            route.localPref <= LOCAL_PREF_PEER || route.localPref == LOCAL_PREF_PEERSTAR  -> BGPRoute.invalid()
+            else -> peerstarRoute(asPath = route.asPath.append(sender))
+        }
+    }
+
+}
+
+object SiblingExtender: Extender<BGPRoute> {
+
+    override fun extend(route: BGPRoute, sender: Node<BGPRoute>): BGPRoute {
+
+        return when {
+            !route.isValid() -> BGPRoute.invalid()
             route === BGPRoute.self() -> customerRoute(siblingHops = 1, asPath = route.asPath.append(sender))
-            else                      -> BGPRoute.with(localPref = route.localPref - 1,
-                                                       asPath = route.asPath.append(sender))
+            else -> BGPRoute.with(localPref = route.localPref - 1,
+                    asPath = route.asPath.append(sender))
         }
     }
 
