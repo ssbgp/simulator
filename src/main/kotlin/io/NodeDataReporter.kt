4231e87bf293da679d2fb266e9e82f0f008fc942
David Fialho <fialho.david@protonmail.com>
2017-10-02 15:55:02 +0100

    Add collector to collect data for each individual node
    Implementing a new collector to add this functionality instead of simply
    adding it to the existing BasicDataCollector provides more flexibility.
    For instance, it makes enabling/disabling this new functionality easy.
    
    The reporter used to report the data collected by the new collector is
    not implemented yet.

---
+++ src/main/kotlin/io/NodeDataReporter.kt
@@ -0,0 +1,15 @@
+package io
+
+import simulation.NodeDataSet
+
+/**
+ * Created on 02-10-2017.
+ *
+ * @author David Fialho
+ */
+class NodeDataReporter {
+
+    fun report(data: NodeDataSet) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
