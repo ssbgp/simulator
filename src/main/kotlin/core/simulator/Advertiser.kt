a59eab5f7ef1809d9ed8a27bdb041bdfc6bf321c
David Fialho <fialho.david@protonmail.com>
2017-11-08 14:31:53 +0000

    Introduce concept of Advertiser

---
+++ src/main/kotlin/core/simulator/Advertiser.kt
@@ -0,0 +1,16 @@
+package core.simulator
+
+/**
+ * Created on 08-11-2017
+ *
+ * @author David Fialho
+ *
+ * An advertiser is some entity that can advertise destinations.
+ */
+interface Advertiser {
+
+    /**
+     * Advertises some destination.
+     */
+    fun advertise()
+}
