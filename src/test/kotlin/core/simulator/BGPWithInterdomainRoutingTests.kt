262ef6f764a3de27aafcaf6e976c262508608cc2
David Fialho <fialho.david@protonmail.com>
2017-11-09 18:14:59 +0000

    Add some integration tests with multiple advertisements

--- src/test/kotlin/core/simulator/BGPWithInterdomainRoutingTests.kt
+++ src/test/kotlin/core/simulator/BGPWithInterdomainRoutingTests.kt
@@ -32,7 +32,7 @@ object BGPWithInterdomainRoutingTests : Spek({
 
         afterEachTest {
             Engine.scheduler.reset()
-            topology.nodes.forEach { it.protocol.reset() }
+            topology.nodes.forEach { it.reset() }
         }
 
         val node = topology.nodes.sortedBy { it.id }
@@ -77,7 +77,7 @@ object BGPWithInterdomainRoutingTests : Spek({
         }
     }
 
-    given("square topology") {
+    given("multiple relationships topology") {
 
         val topology = bgpTopology {
             node { 0 deploying BGP() }
@@ -93,7 +93,7 @@ object BGPWithInterdomainRoutingTests : Spek({
 
         afterEachTest {
             Engine.scheduler.reset()
-            topology.nodes.forEach { it.protocol.reset() }
+            topology.nodes.forEach { it.reset() }
         }
 
         val node = topology.nodes.sortedBy { it.id }
@@ -124,6 +124,114 @@ object BGPWithInterdomainRoutingTests : Spek({
         }
     }
 
+    given("square topology") {
+
+        val topology = bgpTopology {
+            node { 0 deploying BGP() }
+            node { 1 deploying BGP() }
+            node { 2 deploying BGP() }
+            node { 3 deploying BGP() }
+
+            customerLink { 3 to 1 }
+            customerLink { 2 to 0 }
+            customerLink { 2 to 3 }
+            customerLink { 3 to 2 }
+            peerplusLink { 0 to 3 }
+        }
+
+        afterEachTest {
+            Engine.scheduler.reset()
+            topology.nodes.forEach { it.reset() }
+        }
+
+        val node = topology.nodes.sortedBy { it.id }
+        val protocol = node.map { it.protocol as BGP }
+
+        on("simulating with nodes 0 and 1 advertising peer+ routes at 0 and 100") {
+
+            val advertisements = listOf(
+                    Advertisement(node[0], peerplusRoute(), time = 0),
+                    Advertisement(node[1], peerplusRoute(), time = 100)
+            )
+
+            val terminated = Engine.simulate(topology, advertisements, threshold = 1000)
+
+            it("terminated") {
+                assertThat(terminated, Is(true))
+            }
+
+            it("finishes with node 0 selecting peer+ route via himself") {
+                assertThat(protocol[0].routingTable.getSelectedRoute(),
+                        Is(peerplusRoute()))
+                assertThat(protocol[0].routingTable.getSelectedNeighbor(),
+                        Is(node[0]))
+            }
+
+            it("finishes with node 1 selecting peer+ route via himself") {
+                assertThat(protocol[1].routingTable.getSelectedRoute(),
+                        Is(peerplusRoute()))
+                assertThat(protocol[1].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
+            }
+
+            it("finishes with node 2 selecting customer route via node 0") {
+                assertThat(protocol[2].routingTable.getSelectedRoute(),
+                        Is(customerRoute(asPath = pathOf(0))))
+                assertThat(protocol[2].routingTable.getSelectedNeighbor(),
+                        Is(node[0]))
+            }
+
+            it("finishes with node 3 selecting customer route via node 1") {
+                assertThat(protocol[3].routingTable.getSelectedRoute(),
+                        Is(customerRoute(asPath = pathOf(1))))
+                assertThat(protocol[3].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
+            }
+        }
+
+        on("simulating with nodes 0 and 1 advertising customer routes at 0 and 100") {
+
+            val advertisements = listOf(
+                    Advertisement(node[0], customerRoute(), time = 0),
+                    Advertisement(node[1], customerRoute(), time = 100)
+            )
+
+            val terminated = Engine.simulate(topology, advertisements, threshold = 1000)
+
+            it("terminated") {
+                assertThat(terminated, Is(true))
+            }
+
+            it("finishes with node 0 selecting peer+ route via node 3") {
+                assertThat(protocol[0].routingTable.getSelectedRoute(),
+                        Is(peerplusRoute(asPath = pathOf(1, 3))))
+                assertThat(protocol[0].routingTable.getSelectedNeighbor(),
+                        Is(node[3]))
+            }
+
+            it("finishes with node 1 selecting customer route via himself") {
+                assertThat(protocol[1].routingTable.getSelectedRoute(),
+                        Is(customerRoute()))
+                assertThat(protocol[1].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
+            }
+
+            it("finishes with node 2 selecting customer route via node 3") {
+                assertThat(protocol[2].routingTable.getSelectedRoute(),
+                        Is(customerRoute(asPath = pathOf(1, 3))))
+                assertThat(protocol[2].routingTable.getSelectedNeighbor(),
+                        Is(node[3]))
+            }
+
+            it("finishes with node 3 selecting customer route via node 1") {
+                assertThat(protocol[3].routingTable.getSelectedRoute(),
+                        Is(customerRoute(asPath = pathOf(1))))
+                assertThat(protocol[3].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
+            }
+        }
+    }
+
     given("loop topology with customer to destination and peer+ around the cycle") {
 
         val topology = bgpTopology {
@@ -142,10 +250,11 @@ object BGPWithInterdomainRoutingTests : Spek({
 
         afterEachTest {
             Engine.scheduler.reset()
-            topology.nodes.forEach { it.protocol.reset() }
+            topology.nodes.forEach { it.reset() }
         }
 
         val node = topology.nodes.sortedBy { it.id }
+        val protocol = node.map { it.protocol as BGP }
 
         on("simulating with node 0 as the destination") {
 
@@ -153,6 +262,58 @@ object BGPWithInterdomainRoutingTests : Spek({
 
             it("does NOT terminate") {
                 assertThat(terminated, Is(false))
+            }
+        }
+
+
+        on("simulating with nodes 1, 2, and 3 advertising customer routes at time 0") {
+
+            val advertisements = listOf(
+                    Advertisement(node[1], customerRoute()),
+                    Advertisement(node[2], customerRoute()),
+                    Advertisement(node[3], customerRoute())
+            )
+
+            val terminated = Engine.simulate(topology, advertisements, threshold = 1000)
+
+            it("does not terminate") {
+                assertThat(terminated, Is(false))
+            }
+        }
+
+        on("simulating with nodes 1, 2, and 3 advertising customer routes at times 0, 100, and 200") {
+
+            val advertisements = listOf(
+                    Advertisement(node[1], customerRoute(), time = 0),
+                    Advertisement(node[2], customerRoute(), time = 100),
+                    Advertisement(node[3], customerRoute(), time = 200)
+            )
+
+            val terminated = Engine.simulate(topology, advertisements, threshold = 1000)
+
+            it("does not terminate") {
+                assertThat(terminated, Is(true))
+            }
+
+            it("finishes with node 1 selecting a customer route via himself") {
+                assertThat(protocol[1].routingTable.getSelectedRoute(),
+                        Is(customerRoute()))
+                assertThat(protocol[1].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
+            }
+
+            it("finishes with node 2 selecting peer+ route via node 3") {
+                assertThat(protocol[2].routingTable.getSelectedRoute(),
+                        Is(peerplusRoute(asPath = pathOf(1, 3))))
+                assertThat(protocol[2].routingTable.getSelectedNeighbor(),
+                        Is(node[3]))
+            }
+
+            it("finishes with node 3 selecting peer+ route via node 1") {
+                assertThat(protocol[3].routingTable.getSelectedRoute(),
+                        Is(peerplusRoute(asPath = pathOf(1))))
+                assertThat(protocol[3].routingTable.getSelectedNeighbor(),
+                        Is(node[1]))
             }
         }
     }
@@ -173,7 +334,7 @@ object BGPWithInterdomainRoutingTests : Spek({
 
         afterEachTest {
             Engine.scheduler.reset()
-            topology.nodes.forEach { it.protocol.reset() }
+            topology.nodes.forEach { it.reset() }
         }
 
         val node = topology.nodes.sortedBy { it.id }
@@ -221,7 +382,7 @@ object BGPWithInterdomainRoutingTests : Spek({
 
         afterEachTest {
             Engine.scheduler.reset()
-            topology.nodes.forEach { it.protocol.reset() }
+            topology.nodes.forEach { it.reset() }
         }
 
         val node = topology.nodes.sortedBy { it.id }
