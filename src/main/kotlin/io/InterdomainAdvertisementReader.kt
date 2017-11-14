1fae63f170796ea6801f53241c31bc3615942087
David Fialho <fialho.david@protonmail.com>
2017-11-14 17:18:29 +0000

    Refactor InterdomainAdvertisementReader
    Joined ReadHandler and FindHandler to avoid that big duplication of
    code.

--- src/main/kotlin/io/InterdomainAdvertisementReader.kt
+++ src/main/kotlin/io/InterdomainAdvertisementReader.kt
@@ -9,6 +9,11 @@ import utils.toNonNegativeInt
 import java.io.IOException
 import java.io.Reader
 
+
+private val DEFAULT_ADVERTISING_TIME = 0
+private val DEFAULT_DEFAULT_ROUTE = BGPRoute.self()
+
+
 /**
  * Created on 14-11-2017
  *
@@ -16,12 +21,10 @@ import java.io.Reader
  */
 class InterdomainAdvertisementReader(reader: Reader): AutoCloseable {
 
-    companion object {
-        val DEFAULT_ADVERTISING_TIME = 0
-        val DEFAULT_DEFAULT_ROUTE = BGPRoute.self()
-    }
-
-    private class ReadHandler(val advertisements: MutableMap<NodeID, AdvertisementInfo<BGPRoute>>)
+    private class Handler(
+            val advertisements: MutableMap<NodeID, AdvertisementInfo<BGPRoute>>,
+            val advertisers: Map<NodeID, Advertiser<BGPRoute>>? = null
+    )
         : KeyValueParser.Handler {
 
         /**
@@ -43,6 +46,11 @@ class InterdomainAdvertisementReader(reader: Reader): AutoCloseable {
             } catch (e: NumberFormatException) {
                 throw ParseException("advertising node ID must be a non-negative integer value, " +
                         "but was '${entry.key}'", currentLine)
+            }
+
+            // Ignore entry if accepted advertisers are defined
+            if (advertisers != null && advertiserID !in advertisers) {
+                return  // ignore this entry
             }
 
             // The first value is the advertising time - this value is NOT mandatory
@@ -69,61 +77,6 @@ class InterdomainAdvertisementReader(reader: Reader): AutoCloseable {
 
     }
 
-    private class FindHandler(
-            val advertisements: MutableMap<NodeID, Advertisement<BGPRoute>>,
-            val advertisers: Map<NodeID, Advertiser<BGPRoute>>
-    ): KeyValueParser.Handler {
-
-        /**
-         * Invoked when a new entry is parsed.
-         *
-         * @param entry       the parsed entry
-         * @param currentLine line number where the node was parsed
-         */
-        override fun onEntry(entry: KeyValueParser.Entry, currentLine: Int) {
-
-            if (entry.values.size > 2) {
-                throw ParseException("only 2 values are expected for an advertiser, " +
-                        "but ${entry.values.size} were given", currentLine)
-            }
-
-            // The ley corresponds to the advertiser ID
-            val advertiserID = try {
-                entry.key.toNonNegativeInt()
-            } catch (e: NumberFormatException) {
-                throw ParseException("advertising node ID must be a non-negative integer value, " +
-                        "but was '${entry.key}'", currentLine)
-            }
-
-            // Get the advertiser if available, else ignore this entry
-            val advertiser = advertisers.getOrElse(advertiserID) {
-                return  // ignore this entry
-            }
-
-            // The first value is the advertising time - this value is NOT mandatory
-            // The KeyValueParser ensure that there is at least one value always, even if it is blank
-            val timeValue = entry.values[0]
-            val time = if (timeValue.isBlank()) DEFAULT_ADVERTISING_TIME else try {
-                timeValue.toNonNegativeInt()
-            } catch (e: NumberFormatException) {
-                throw ParseException("advertising time must be a non-negative integer value, " +
-                        "but was '$timeValue'", currentLine)
-            }
-
-            // The second value is a cost label for the default route's local preference - this value is NOT mandatory
-            val defaultRoute = if (entry.values.size == 1 || entry.values[1].isBlank()) {
-                DEFAULT_DEFAULT_ROUTE
-            } else {
-                BGPRoute.with(parseInterdomainCost(entry.values[1], currentLine), pathOf())
-            }
-
-            if (advertisements.putIfAbsent(advertiserID, Advertisement(advertiser, defaultRoute, time)) != null) {
-                throw ParseException("advertiser $advertiserID is defined twice", currentLine)
-            }
-        }
-
-    }
-
     private val parser = KeyValueParser(reader)
 
     /**
@@ -136,7 +89,7 @@ class InterdomainAdvertisementReader(reader: Reader): AutoCloseable {
     @Throws(ParseException::class, IOException::class)
     fun read(): Map<NodeID, AdvertisementInfo<BGPRoute>> {
         val advertisements = HashMap<NodeID, AdvertisementInfo<BGPRoute>>()
-        parser.parse(ReadHandler(advertisements))
+        parser.parse(Handler(advertisements))
         return advertisements
     }
 
@@ -147,11 +100,19 @@ class InterdomainAdvertisementReader(reader: Reader): AutoCloseable {
      */
     @Throws(ParseException::class, IOException::class)
     fun find(advertisers: List<Advertiser<BGPRoute>>): List<Advertisement<BGPRoute>> {
-        val advertisements = HashMap<NodeID, Advertisement<BGPRoute>>()
+        val advertisements = HashMap<NodeID, AdvertisementInfo<BGPRoute>>()
         val advertisersByID = advertisers.associateBy({ it.id }, { it })
 
-        parser.parse(FindHandler(advertisements, advertisersByID))
-        return advertisements.values.toList()
+        parser.parse(Handler(advertisements, advertisersByID))
+
+        return advertisers.map {
+            val info = advertisements[it.id]
+            Advertisement(
+                    advertiser = it,
+                    route = info?.defaultRoute ?: DEFAULT_DEFAULT_ROUTE,
+                    time = info?.time ?: DEFAULT_ADVERTISING_TIME
+            )
+        }
     }
 
     /**
