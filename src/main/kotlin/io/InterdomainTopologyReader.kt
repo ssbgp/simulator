8868afdbf6488f5e09d0068094b245d5d27fcfbe
David Fialho <fialho.david@protonmail.com>
2017-11-14 14:29:28 +0000

    Re-implement InterdomainTopologyReader to use the new TopologyParser

--- src/main/kotlin/io/InterdomainTopologyReader.kt
+++ src/main/kotlin/io/InterdomainTopologyReader.kt
@@ -2,7 +2,6 @@ package io
 
 import bgp.*
 import core.routing.*
-import io.TopologyParser.Handler
 import utils.toNonNegativeInt
 import java.io.*
 
@@ -11,7 +10,7 @@ import java.io.*
  *
  * @author David Fialho
  */
-class InterdomainTopologyReader(reader: Reader): TopologyReader<BGPRoute>, Closeable, Handler {
+class InterdomainTopologyReader(reader: Reader): TopologyReader<BGPRoute>, Closeable {
 
     /**
      * Provides option to create a reader with a file object.
@@ -19,8 +18,75 @@ class InterdomainTopologyReader(reader: Reader): TopologyReader<BGPRoute>, Close
     @Throws(FileNotFoundException::class)
     constructor(file: File): this(FileReader(file))
 
-    private val builder = TopologyBuilder<BGPRoute>()
     private val parser = TopologyParser(reader)
+
+    private class InterdomainHandler(val builder: TopologyBuilder<BGPRoute>): TopologyParser.Handler {
+
+        /**
+         * Invoked when reading the stream when a new node item is read.
+         *
+         * @param id          the ID of the node parse
+         * @param values      sequence of values associated with the node
+         * @param currentLine line number where the node was parsed
+         */
+        override fun onNodeItem(id: NodeID, values: List<String>, currentLine: Int) {
+
+            if (values.size < 2) {
+                throw ParseException("node is missing required values: Protocol and/or MRAI", currentLine)
+            }
+
+            val protocolLabel = values[0]
+            val mrai = try {
+                values[1].toNonNegativeInt()
+            } catch (e: NumberFormatException) {
+                throw ParseException("MRAI must be a non-negative integer number, but was ${values[1]}", currentLine)
+            }
+
+            val protocol = when (protocolLabel.toLowerCase()) {
+                "bgp" -> BGP(mrai)
+                "ssbgp" -> SSBGP(mrai)
+                "issbgp" -> ISSBGP(mrai)
+                "ssbgp2" -> SSBGP2(mrai)
+                "issbgp2" -> ISSBGP2(mrai)
+                else -> throw ParseException(
+                        "protocol label `$protocolLabel` was not recognized: supported labels are BGP, " +
+                                "SSBGP, ISSBGP, SSBGP2, and ISSBGP2", currentLine)
+            }
+
+            try {
+                builder.addNode(id, protocol)
+
+            } catch (e: ElementExistsException) {
+                throw ParseException(e.message!!, currentLine)
+            }
+        }
+
+        /**
+         * Invoked when reading the stream when a new link item is read.
+         *
+         * @param tail        the ID of the tail node
+         * @param head        the ID of the head node
+         * @param values      sequence of values associated with the link item
+         * @param currentLine line number where the node was parsed
+         */
+        override fun onLinkItem(tail: NodeID, head: NodeID, values: List<String>, currentLine: Int) {
+
+            if (values.isEmpty()) {
+                throw ParseException("link is missing extender label value", currentLine)
+            }
+
+            val extender = parseInterdomainExtender(values[0], currentLine)
+
+            try {
+                builder.link(tail, head, extender)
+
+            } catch (e: ElementNotFoundException) {
+                throw ParseException(e.message!!, currentLine)
+            } catch (e: ElementExistsException) {
+                throw ParseException(e.message!!, currentLine)
+            }
+        }
+    }
 
     /**
      * Returns a Topology object that is represented in the input source.
@@ -33,73 +99,9 @@ class InterdomainTopologyReader(reader: Reader): TopologyReader<BGPRoute>, Close
      */
     @Throws(IOException::class, ParseException::class)
     override fun read(): Topology<BGPRoute> {
-        parser.parse(this)
+        val builder = TopologyBuilder<BGPRoute>()
+        parser.parse(InterdomainHandler(builder))
         return builder.build()
-    }
-
-    /**
-     * Invoked when reading the stream when a new node item is read.
-     *
-     * @param id          the ID of the node parse
-     * @param values      sequence of values associated with the node
-     * @param currentLine line number where the node was parsed
-     */
-    override fun onNodeItem(id: NodeID, values: List<String>, currentLine: Int) {
-
-        if (values.size < 2) {
-            throw ParseException("Node is missing required values: Protocol and/or MRAI", currentLine)
-        }
-
-        val protocolLabel = values[0]
-        val mrai = try {
-            values[1].toNonNegativeInt()
-        } catch (e: NumberFormatException) {
-            throw ParseException("Failed to parse `${values[1]}`: must be a non-negative integer number", currentLine)
-        }
-
-        val protocol = when (protocolLabel.toLowerCase()) {
-            "bgp" -> BGP(mrai)
-            "ssbgp" -> SSBGP(mrai)
-            "issbgp" -> ISSBGP(mrai)
-            "ssbgp2" -> SSBGP2(mrai)
-            "issbgp2" -> ISSBGP2(mrai)
-            else -> throw ParseException(
-                    "Protocol label `$protocolLabel` was not recognized: supported labels are BGP, " +
-                    "SSBGP, ISSBGP, SSBGP2, and ISSBGP2", currentLine)
-        }
-
-        try {
-            builder.addNode(id, protocol)
-
-        } catch (e: ElementExistsException) {
-            throw ParseException(e.message!!, currentLine)
-        }
-    }
-
-    /**
-     * Invoked when reading the stream when a new link item is read.
-     *
-     * @param tail        the ID of the tail node
-     * @param head        the ID of the head node
-     * @param values      sequence of values associated with the link item
-     * @param currentLine line number where the node was parsed
-     */
-    override fun onLinkItem(tail: NodeID, head: NodeID, values: List<String>, currentLine: Int) {
-
-        if (values.isEmpty()) {
-            throw ParseException("Link is missing required values: extender label", currentLine)
-        }
-
-        val extender = parseInterdomainExtender(values[0], currentLine)
-
-        try {
-            builder.link(tail, head, extender)
-
-        } catch (e: ElementNotFoundException) {
-            throw ParseException(e.message!!, currentLine)
-        } catch (e: ElementExistsException) {
-            throw ParseException(e.message!!, currentLine)
-        }
     }
 
     /**
