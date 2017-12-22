1606d4f2c31aed6aaffe8d0d998adcb2786998a9
David Fialho <fialho.david@protonmail.com>
2017-12-22 12:43:26 +0000

    Document some components in 'core.simulator' package

--- src/main/kotlin/core/simulator/AdvertiseEvent.kt
+++ src/main/kotlin/core/simulator/AdvertiseEvent.kt
@@ -6,12 +6,12 @@ import core.routing.Route
  * Created on 09-11-2017
  *
  * @author David Fialho
+ *
+ * The advertise event triggers the [Advertiser.advertise] method of [advertiser] to have it
+ * advertise [route].
  */
 class AdvertiseEvent<R: Route>(private val advertiser: Advertiser<R>, private val route: R): Event {
 
-    /**
-     * Processes this event.
-     */
     override fun processIt() {
         advertiser.advertise(route)
     }
